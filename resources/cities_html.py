46821b7cd0db3d032be6c165b1e64e193c4db65b
Juliann Pezzullo <jump8621@gmail.com>
2020-11-14 16:23:54 -0500

    add data

--- resources/cities_html.py
+++ resources/cities_html.py
@@ -1,19 +1,21 @@
 
 import pandas as pd
 
-df = pd.read_csv('resources\cities.csv')
+df = pd.read_csv('cities.csv')
 
 
-cities_html = df.to_html(index=False) 
+cities_html = df.to_html("cities.html", index=False) 
 
 print(cities_html)  
 
+cities_html = df.to_html(classes='table table-responsive'))
+# print
 # <table class="cities_html.dataframe">
     
 # </table>
 
 
-# HTML(df.to_html(classes='table table-striped'))
+# HTML(df.to_html(classes='table table-striped')) - this would work
 # cities.to_html('city.html', index=False)
 # <div class="table-responsive">
 #     <table class="table">
